38eb2fd2194c3b428e31af0356d2d77c397954c2
Zayn <zrassam@hotmail.co.uk>
2023-03-24 22:42:54 +0000

    second draft - much better when using display: grid

--- javascript.js
+++ javascript.js
@@ -1,13 +1,20 @@
-function createGrid(size){
-    size = Math.floor(size)
-    for (let i = 0; i<size**2; i++){
+function createGrid(){
+
+    const myNode = document.getElementById("main");
+    myNode.innerHTML = '';
+
+    const sliderValue = document.querySelector(".slider").value;
+
+    const main = document.querySelector("#main");
+    const col = "1fr ";
+    main.style.gridTemplateColumns = `${col.repeat(sliderValue)}`;
+    
+
+    for (let i = 0; i<sliderValue**2; i++){
         let div = document.createElement("div")
-        // div.style.width = "100px";
-        // div.style.height = "100px";
-        div.style.background = "red";
+        div.style.background = "white";
         div.style.color = "white";
         div.style.opacity = "1";
-        div.innerHTML = i+1;
         div.setAttribute("class", "square")
         document.getElementById("main").appendChild(div);
 
@@ -16,31 +23,79 @@ function createGrid(size){
     }
 }
 
-function getUserInput() {
-    let input = document.querySelector("input");
-    if (input.value < 1 || input.value > 10){
-        alert("please type a number between 0-10");
-    }
-    else {
-        const myNode = document.getElementById("main");
-        myNode.innerHTML = '';
-        createGrid(input.value);
-    }    
-    document.querySelector("input").value = "";
-}
-
-const button = document.querySelector("button");
-button.addEventListener("click", getUserInput);
-
-createGrid(10);
-
 
 function hoverFunc(){
-    const randomColor = Math.floor(Math.random()*16777215).toString(16);
-    let colour = "#" + randomColor;
-    this.style.background=colour;
-    this.style.opacity -= '0.2';
+    if (!eraserButtonPressed) {
+        if (rainbowButtonPressed){
+            const randomColor = Math.floor(Math.random()*16777215).toString(16);
+            let colour = "#" + randomColor;
+            this.style.background=colour;
+        }
+        else {
+            this.style.background=colourWheel.value;
+        }
+    } else{
+        let colour = "#ededed";
+        this.style.background="white";
+    }
 }
 
 
+const slider = document.querySelector(".slider");
+slider.addEventListener("mouseup", createGrid);
+createGrid(slider.value);
 
+
+function updateSliderText(){
+    const sliderValue = slider.value;
+    const gridSizeText = document.querySelector(".grid-size");
+    let grid = `${sliderValue} x ${sliderValue}`;
+    gridSizeText.textContent = grid; 
+}
+
+slider.addEventListener("input", updateSliderText)
+updateSliderText();
+
+
+const clear = document.querySelector(".clear");
+clear.addEventListener("click", createGrid);
+
+
+
+function rainbowSwitch(){
+    if (rainbowButtonPressed==false){
+        rainbow.classList.add("rainbow-bg");
+        rainbowButtonPressed=true;
+    }else{
+        rainbow.classList.remove("rainbow-bg");
+        rainbowButtonPressed=false;
+    }
+}
+
+let rainbowButtonPressed = false;
+const rainbow = document.querySelector(".rainbow-button");
+rainbow.addEventListener("click", rainbowSwitch);
+
+
+
+function eraserSwitch(){
+    if (eraserButtonPressed==false){
+        eraserButtonPressed=true;
+        eraser.classList.add("button-clicked")
+    }else{
+        eraserButtonPressed=false;
+        eraser.classList.remove("button-clicked")
+    }
+}
+let eraserButtonPressed = false;
+const eraser = document.querySelector(".eraser");
+eraser.addEventListener("click", eraserSwitch);
+
+
+
+function changeColour(){
+    console.log(colourWheel.value);
+}
+let colourWheel = document.querySelector(".colour-wheel");
+colourWheel.value='#00000';
+colourWheel.addEventListener('click', changeColour);
